e34f8d27f28fb354d6d301d5578b90cce0749781
Oyek Mirzokulov <mirzokulovoyek90@gmail.com>
2025-10-06 17:34:52 +0500

    good

---
+++ museum_label.py
@@ -0,0 +1,22 @@
+artifact = input("Artifact Name: ")
+period = input("Hestorical Period: ")
+origin = input("Contry of Orign: ")
+acquired = input("Year of Acquistio: ")
+mwing = input("Museum Wing: ")
+
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print(" THE NATIONAL MUSEUM OF HISTORY ")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
+
+print("Artifact: ",artifact)
+print("Origin: ",origin)
+print("Period: ",period)
+print("Acquired: ",acquired)
+
+print("\n")
+
+print("Catalog Number: ACQ-",acquired,"-",origin )
+
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+print("Location: ",mwing)
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
