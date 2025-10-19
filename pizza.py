1ec5045fde607f583852e3e371ca55498d598091
Oybekx7 <mirzokulovoybek90@gmail.com>
2025-10-19 19:00:25 +0500

    Update pizza.py

--- pizza.py
+++ pizza.py
@@ -5,7 +5,7 @@ print("Type 'done' when finished ordering")
 total = 0
 
 while True:
-    size = input("Enter pizza size: ")
+    size = input("\nEnter pizza size: ")
     if size == "done":
         break
 
@@ -16,23 +16,20 @@ while True:
     elif size == "family":
         prices = 18.00
     else:
-        print("Invalid size! Please enter personal, medium or family. \n")
+        print("\nInvalid size! Please enter personal, medium or family.\n")
         continue
-
     total += prices
     print(f"Price: ${prices:.2f}")
-    print(f"Current total: ${total:.2f} \n")
+    print(f"Current total: ${total:.2f}")
 
-discount = 0
-if total > 39:
-    discount = 5
-final_total = total - discount 
-
-print("=== Order Summary ===")
+print("\n=== Order Summary ===")
 print(f"Subtotal: ${total:.2f}")
-if discount > 0:
-    print(f"Party Order Discount: -${discount:.2f}")
+if total > 39:
+    print(f"Party Order Discount: -$5.00")
 else:
-    print(f"Party Order Discount: 0")
-print(F"Final Total: ${final_total:.2f}")
-print("Thank you your order!")
+    print(f"Party Order Discount: $0.00")
+if total > 39:
+    print(f"Final Total: ${total - 5}")
+else:
+    print(f"Final Total: ${total}")
+print("Thank you for your order!")
